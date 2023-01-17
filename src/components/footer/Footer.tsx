1f5afbe9106a777727a96c59854056b35b97c8dd
Pavel <pavel07007@gmail.com>
2023-01-17 22:26:33 +0300

    change footer.module.css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from './footer.module.css'
 
 export const Footer = () => {
     return (
-        <div>
-            Hello
-        </div>
-    );
+        <footer className={styles.footerWrapper}>
+            <div className={styles.footerContent}>
+                <h3>Павел Шилин</h3>
+                <div className={styles.socialContainer}>
+                    <div className={styles.social}></div>
+                    <div className={styles.social}></div>
+                    <div className={styles.social}></div>
+                    <div className={styles.social}></div>
+                </div>
+               <p> @ 2023 Все права защищены</p>
+            </div>
+        </footer>
+    )
 };
 
