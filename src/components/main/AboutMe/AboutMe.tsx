13153dc2a992ed9578d09b05eff7abd95c701c50
Pavel <pavel07007@gmail.com>
2023-01-17 22:52:21 +0300

    change styles

--- src/components/main/AboutMe/AboutMe.tsx
+++ src/components/main/AboutMe/AboutMe.tsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     return (
         <div className={style.aboutMe}>
             <div className={style.aboutMeContent}>
-                <div>
+                <div className={style.aboutMeTextContainer}>
                     <p className={style.aboutMeText}>Привет! Меня зовут Павел Шилин. Я Front-end разработчик</p>
                 </div>
                 <img src={me}  alt="" className={style.avatar}/>
