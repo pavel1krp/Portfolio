c561ded56205b9d3a08bad2563b5fb542921e672
Pavel <pavel07007@gmail.com>
2023-01-17 22:15:49 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
 import {Main} from "./components/main/Main";
+import {Footer} from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-        <div className={'Wrapper'}>
-      <Header/>
-      <Main />
+    return (
+        <div className="App">
+            <div className={'Wrapper'}>
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
