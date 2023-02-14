c7b37fd046a90cb253ef1aa6e95284d0a16a605d
Pavel <pavel07007@gmail.com>
2023-02-14 19:33:37 +0300

    change styles

--- src/components/main/AboutMe/AboutMe.tsx
+++ src/components/main/AboutMe/AboutMe.tsx
@@ -4,15 +4,17 @@ import me from '../../../Assets/Images/myPhoto.jpg'
 
 const AboutMe = () => {
     return (
-        <div className={style.aboutMe}>
-            <div className={style.aboutMeContent}>
-                <div className={style.aboutMeTextContainer}>
-                    <h5 className={style.aboutMeText}>HELLO, MY NAME IS</h5>
-                    <h1 className={style.myName}>Pavel Shilin</h1>
-                    <h5 className={style.aboutMeTextSmall}>I am a front-end developer </h5>
+        <div className={style.container}>
+            <div className={style.aboutMe}>
+                <div className={style.aboutMeContent}>
+                    <div className={style.aboutMeTextContainer}>
+                        <h5 className={style.aboutMeText}>HELLO, MY NAME IS</h5>
+                        <h1 className={style.myName}>Pavel Shilin</h1>
+                        <h5 className={style.aboutMeTextSmall}>I am a front-end developer </h5>
 
+                    </div>
+                    <img src={me} alt="" className={style.avatar}/>
                 </div>
-                <img src={me} alt="" className={style.avatar}/>
             </div>
         </div>
     );
