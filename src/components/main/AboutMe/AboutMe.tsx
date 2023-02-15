6ae6dc8a3c646f1826215ee12f509ff429af5425
Pavel <pavel07007@gmail.com>
2023-02-15 15:00:20 +0300

    change structure and styles

--- src/components/main/AboutMe/AboutMe.tsx
+++ src/components/main/AboutMe/AboutMe.tsx
@@ -5,16 +5,16 @@ import me from '../../../Assets/Images/myPhoto.jpg'
 const AboutMe = () => {
     return (
         <section className={style.aboutMeWrapper}>
-            <div className="container"></div>
-            <div className={style.aboutMe}>
-                <div className={style.aboutMeContent}>
-                    <div className={style.aboutMeTextContainer}>
-                        <h5 className={style.aboutMeText}>HELLO, MY NAME IS</h5>
-                        <h1 className={style.myName}>Pavel Shilin</h1>
-                        <h5 className={style.aboutMeTextSmall}>I am a front-end developer </h5>
-
+            <div className="container">
+                <div className={style.aboutMe}>
+                    <div className={style.aboutMe}>
+                        <div className={style.aboutMeTextContainer}>
+                            <h5 className={style.aboutMeText}>Welcome</h5>
+                            <h1 className={style.myName}>MY NAME IS Pavel Shilin</h1>
+                            <h5 className={style.aboutMeTextSmall}>I am a front-end developer </h5>
+                        </div>
+                        <img src={me} alt="" className={style.avatar}/>
                     </div>
-                    <img src={me} alt="" className={style.avatar}/>
                 </div>
             </div>
         </section>
