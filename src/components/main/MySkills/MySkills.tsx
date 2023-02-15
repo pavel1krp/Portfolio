cefdba56ad25865e2539965325d2a01611bec480
Pavel <pavel07007@gmail.com>
2023-02-15 18:32:23 +0300

    change skills structure

--- src/components/main/MySkills/MySkills.tsx
+++ src/components/main/MySkills/MySkills.tsx
@@ -3,22 +3,21 @@ import {SkillsCard} from "./SkillsCard/SkillsCard";
 import styles from "./MySkills.module.scss"
 import {Title} from "../../Common/Title";
 import {skills} from "./SkillsCard/skills";
-export const MySkills = ()=> {
-    const mappedSkills = skills.map(el=>{
-        return(
-            <SkillsCard key={el.ID} skills={el} />
+
+export const MySkills = () => {
+    const mappedSkills = skills.map(el => {
+        return (
+            <SkillsCard key={el.ID} skills={el}/>
         )
     })
-        return (
-            <section className={styles.mySkillsContainer}>
-                {/*<div className={styles.mySkills}>*/}
-                {/*    <div className={styles.mySkillsTop}>*/}
-                       <Title title={'My skills'}/>
-                {/*    </div>*/}
-                {/*    <div className={styles.mySkillsBottom}>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                {mappedSkills}
-            </section>
-        );
+    return (
+        <section>
+            <div className={'container'}>
+                <Title title={'My skills'}/>
+                <div className={styles.skills}>
+                    {mappedSkills}
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/components/main/MySkills/SkillsCard/SkillsCard.tsx
+++ src/components/main/MySkills/SkillsCard/SkillsCard.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './SkillsCard.module.scss'
 import {SkillsType} from "./skills";
+
 export type SkillsCardPropsType = {
     skills: SkillsType
 }
-export const SkillsCard = (props:SkillsCardPropsType ) => {
+export const SkillsCard = (props: SkillsCardPropsType) => {
     const {skills, ...RestProps} = props;
     const {Icons, title, color} = skills
     return (
-        <div className={styles.cardContainer}>
-            <Icons size={50} color={color} />
-        </div>
+            <div className={styles.cardContainer}>
+                <div className={styles.skillsContainer}>
+                    <Icons size={60} color={color}/>
+                </div>
+                <p className={styles.skillsName}>{title}</p>
+            </div>
     );
 };
 
