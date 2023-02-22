f03a39dd099226572cb494f095e2ecb3bdd042b7
Pavel <pavel07007@gmail.com>
2023-02-22 18:50:21 +0300

    chnage styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,15 +4,17 @@ import styles from './footer.module.scss'
 export const Footer = () => {
     return (
         <footer className={styles.footerWrapper}>
-            <div className={styles.footerContent}>
-                <h3 className={styles.text}>Павел Шилин</h3>
-                <div className={styles.socialContainer}>
-                    <div className={styles.social}></div>
-                    <div className={styles.social}></div>
-                    <div className={styles.social}></div>
-                    <div className={styles.social}></div>
+            <div className='container'>
+                <div className={styles.footerContent}>
+                    {/*<h3 className={styles.text}>Павел Шилин</h3>*/}
+                    <div className={styles.socialContainer}>
+                        <div className={styles.social}></div>
+                        <div className={styles.social}></div>
+                        <div className={styles.social}></div>
+                        <div className={styles.social}></div>
+                    </div>
+                    <p className={styles.text}>Copyright &#169; 2023</p>
                 </div>
-               <p className={styles.text}> &#169; 2023 Все права защищены</p>
             </div>
         </footer>
     )
