b43e58a4d01d99c255d6681aaceb190c03669e7d
Pavel <pavel07007@gmail.com>
2023-02-23 18:13:29 +0300

    changq styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,14 +6,10 @@ export const Footer = () => {
         <footer className={styles.footerWrapper}>
             <div className='container'>
                 <div className={styles.footerContent}>
-                    {/*<h3 className={styles.text}>Павел Шилин</h3>*/}
-                    <div className={styles.socialContainer}>
-                        <div className={styles.social}></div>
-                        <div className={styles.social}></div>
-                        <div className={styles.social}></div>
-                        <div className={styles.social}></div>
-                    </div>
                     <p className={styles.text}>Copyright &#169; 2023</p>
+                    <div className={styles.contactFooter}>
+
+                    </div>
                 </div>
             </div>
         </footer>
