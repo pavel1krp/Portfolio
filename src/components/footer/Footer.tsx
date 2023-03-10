490c0a1210cf230c680b07cd1dbf70964bb3abcc
Pavel <pavel07007@gmail.com>
2023-03-10 20:04:26 +0300

    add footer link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,13 +6,11 @@ export const Footer = () => {
         <footer className={styles.footerWrapper}>
             <div className='container'>
                 <div className={styles.footerContent}>
-                    <div>
-                    </div>
                     <p className={styles.text}>Copyright &#169; 2023</p>
                     <div className={styles.contactFooter}>
-                        <a className={styles.link} href="">GITHUB</a>
-                        <a className={styles.link} href="">TELEGRAM</a>
-                        <a className={styles.link} href="">LINKEDIN</a>
+                        <a className={styles.link} href="https://github.com/pavel1krp">GITHUB</a>
+                        <a className={styles.link} href="https://t.me/PavelShylin">TELEGRAM</a>
+                        <a className={styles.link} href="https://www.linkedin.com/in/%D0%BF%D0%B0%D0%B2%D0%B5%D0%BB-%D1%88%D0%B8%D0%BB%D0%B8%D0%BD-883ba1262/">LINKEDIN</a>
                     </div>
                 </div>
             </div>
