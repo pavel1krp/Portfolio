cd062dc23b21c585782dfedf22de79a061dd040a
Pavel <pavel07007@gmail.com>
2023-03-12 20:23:57 +0300

    change link styles

--- src/components/main/MyWork/MyWorkItem/MyWorkItem.tsx
+++ src/components/main/MyWork/MyWorkItem/MyWorkItem.tsx
@@ -12,7 +12,7 @@ export const MyWorkItem = (props:MyWorkItemPropsType) => {
     return (
         <div className={styles.MyWorkItem}>
             <div className={styles.backgroundDiv}>
-                <button className={styles.MyWorkSeeButton}><a href={props.link}>Open</a></button>
+                <button className={styles.MyWorkSeeButton}><a className={styles.link} href={props.link}>Open</a></button>
             </div>
             <div className={styles.descriptionDiv}>
                 <h3 className={styles.projectName}>{projectName}</h3>
