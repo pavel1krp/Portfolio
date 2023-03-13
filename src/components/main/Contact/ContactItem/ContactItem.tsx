f8c7cf1b713cf13d2b4aa03ccce1e13fd32b30f4
Pavel <pavel07007@gmail.com>
2023-03-13 17:42:47 +0300

    refactor

--- src/components/main/Contact/ContactItem/ContactItem.tsx
+++ src/components/main/Contact/ContactItem/ContactItem.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import style from '../contact.module.scss'
 
-type ContactItemPropsType ={
-    title:string
-    description:string
-    icon:any
+type ContactItemPropsType = {
+    title: string
+    description: string
+    icon: any
 }
 
-export const ContactItem = (props:ContactItemPropsType) => {
+export const ContactItem = (props: ContactItemPropsType) => {
     return (
         <div className={style.contactItemContainer}>
             <div className={style.contactImage}>
-                <props.icon size={30} />
+                <props.icon size={30}/>
             </div>
             <div className={style.contactText}>
                 <h4 className={style.contactName}>{props.title}</h4>
