ae7ee9163aa6e01a559b44dc6c90ae4dacf21e9e
Pavel <pavel07007@gmail.com>
2023-03-14 22:33:34 +0300

    add background fo workItem

--- src/components/main/MyWork/MyWork.tsx
+++ src/components/main/MyWork/MyWork.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import {MyWorkItem} from "./MyWorkItem/MyWorkItem";
 import styles from "./MyWork.module.scss"
 import {Title} from "../../Common/Title";
+import Social from '../../../Assets/Images/social.jpg'
+
 const MyWork = () => {
+    const social = {
+        color: 'blue',
+        backgroundImage: 'url(' + Social + ')',
+    };
+
     return (
         <section id={'projects'} className={styles.myWorkWrapper}>
             <div className={styles.myWork}>
@@ -11,12 +18,16 @@ const MyWork = () => {
                 </div>
                 <div className={styles.myWorkBottom}>
                     <div className={styles.rightWork}>
-                        <MyWorkItem link={'https://pavel1krp.github.io/samurai-way/'} projectName={'Social Network'} projectDescription={'Social Network'}/>
-                        <MyWorkItem link={'https://pavel1krp.github.io/samurai-way/'} projectName={'Social Network'} projectDescription={'Social Network'}/>
+                        <MyWorkItem style={social} link={'https://pavel1krp.github.io/samurai-way/'} projectName={'Social Network'}
+                                    projectDescription={'Social Network'}/>
+                        <MyWorkItem style={social} link={'https://pavel1krp.github.io/samurai-way/'} projectName={'Social Network'}
+                                    projectDescription={'Social Network'}/>
                     </div>
                     <div className={styles.leftWork}>
-                        <MyWorkItem link={'https://pavel1krp.github.io/samurai-way/'} projectName={'TodoList'} projectDescription={'TodoList'}/>
-                        <MyWorkItem link={'https://pavel1krp.github.io/samurai-way/'} projectName={'TodoList'} projectDescription={'TodoList'}/>
+                        <MyWorkItem style={social} link={'https://pavel1krp.github.io/samurai-way/'} projectName={'TodoList'}
+                                    projectDescription={'TodoList'}/>
+                        <MyWorkItem style={social} link={'https://pavel1krp.github.io/samurai-way/'} projectName={'TodoList'}
+                                    projectDescription={'TodoList'}/>
                     </div>
                 </div>
             </div>
--- src/components/main/MyWork/MyWorkItem/MyWorkItem.tsx
+++ src/components/main/MyWork/MyWorkItem/MyWorkItem.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './MyWorkItem.module.scss'
+import Social from "*.jpg";
 
+type StyleType = {
+    color: string
+    backgroundImage: string,
+}
 type MyWorkItemPropsType = {
     projectName:string
     projectDescription:string
     link:string
+    style: StyleType
+
 }
 
 export const MyWorkItem = (props:MyWorkItemPropsType) => {
     const{projectName,projectDescription, ...restProps }= props
+
+
     return (
         <div className={styles.MyWorkItem}>
-            <div className={styles.backgroundDiv}>
+            <div className={styles.backgroundDiv} style={props.style}>
                 <button className={styles.MyWorkSeeButton}><a className={styles.link} href={props.link}>Open</a></button>
             </div>
             <div className={styles.descriptionDiv}>
